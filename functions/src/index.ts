c808b134ca24a0139149163efb787f94a7777013
Michal Vala <michal.vala@gmail.com>
2025-02-14 09:40:55 +0100

    checks to not duplicate the data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,9 @@ import { onRequest, HttpsError } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
 import admin from "firebase-admin";
+import { FileMetadata } from "@google-cloud/storage";
 import { racemate } from "racemate-msg";
+import { DocumentReference } from "firebase-admin/firestore";
 
 let adminApp: admin.app.App;
 
@@ -22,8 +24,9 @@ if (admin.apps.length === 0) {
 }
 
 const db = adminApp.firestore();
+const LAP_COLLECTION = "laps";
 
-export const hello = onRequest(
+export const unauthorizedUpload = onRequest(
   {
     region: ["europe-west3"],
   },
@@ -42,10 +45,38 @@ export const hello = onRequest(
     const data = new Uint8Array(req.rawBody);
     const lap = racemate.Lap.deserialize(data);
 
-    const bucket = admin.storage().bucket();
     const filename = `laps/${lap.player_name}_${lap.player_surname}/${lap.track}_${lap.car_model}/${lap.timestamp}.lap`;
-    const file = bucket.file(filename);
-    const writeStream = await file.createWriteStream({
+    const metadata = await uploadFile(filename, data);
+
+    const queryResult = await db
+      .collection(LAP_COLLECTION)
+      .where("fileFireStorage", "==", metadata.mediaLink)
+      .get();
+    if (queryResult.size == 0) {
+      try {
+        const docRef = await storeMetadata(metadata, lap);
+        logger.log("Document added", docRef);
+        res.status(200).send(`Lap recorded: ${docRef}`);
+      } catch (error) {
+        logger.error("Error adding data:", error);
+        throw new HttpsError("internal", "Error adding data to Firestore");
+      }
+    } else {
+      logger.log("we already have this lap recorded");
+      res.status(409).send("We already have this lap.");
+    }
+  }
+);
+
+const uploadFile = async (
+  filename: string,
+  data: Uint8Array
+): Promise<FileMetadata> => {
+  const bucket = admin.storage().bucket();
+  const file = bucket.file(filename);
+  const [fileExists] = await file.exists();
+  if (!fileExists) {
+    const writeStream = file.createWriteStream({
       metadata: {
         contentType: "application/octet-stream",
       },
@@ -55,24 +86,27 @@ export const hello = onRequest(
       writeStream.on("finish", resolve);
       writeStream.on("error", reject);
     });
-    const [metadata] = await file.getMetadata();
-    logger.log("File saved", metadata);
-
-    try {
-      const docRef = await db.collection("laps").add({
-        fileFireStorage: metadata.mediaLink,
-        name: lap.player_name + " " + lap.player_surname,
-        track: lap.track,
-        laptime: lap.lap_time_ms,
-        car: lap.car_model,
-        timestamp: lap.timestamp,
-      });
-      logger.log("Document added", docRef);
-      res.status(200).send(`Document added: ${docRef}`);
-    } catch (error) {
-      logger.error("Error adding data:", error);
-      throw new HttpsError("internal", "Error adding data to Firestore");
-    }
-    logger.log(4);
+    logger.log("File saved", filename);
+  } else {
+    logger.log("file already there", filename);
   }
-);
+  const [metadata] = await file.getMetadata();
+  logger.log("file metadata", metadata);
+  return metadata;
+};
+
+const storeMetadata = async (
+  metadata: FileMetadata,
+  lap: racemate.Lap
+): Promise<DocumentReference> => {
+  const docRef = await db.collection(LAP_COLLECTION).add({
+    fileFireStorage: metadata.mediaLink,
+    name: lap.player_name + " " + lap.player_surname,
+    track: lap.track,
+    laptime: lap.lap_time_ms,
+    car: lap.car_model,
+    timestamp: lap.timestamp,
+  });
+  logger.log("Document added", docRef);
+  return docRef;
+};
